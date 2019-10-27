276b6a4f5f6cf22e6c99dea3e0640a7ec95956ac
Max Goodman <max.goodman@flatironschool.com>
2019-10-27 08:54:03 -0400

    Create Units.sol

---
+++ Units.sol
@@ -0,0 +1,13 @@
+pragma solidity ^0.4.0;
+
+contract SimpleContract{
+   //ether szabo finney wei 
+   bool isEther = (1 ether == 4000 finney);
+   
+   //time units
+   bool isTime(1 = 1 seconds);
+   
+   //seconds, hours, days, weeks
+   bool isTime(1 hours == 60 minutes);
+   
+}
